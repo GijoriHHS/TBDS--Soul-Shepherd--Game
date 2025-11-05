f4905abd25cc720dfd633e06ad7d6cdea806921a
gihaagse <gihaagse@gmail.com>
2025-11-05 23:12:22 +0100

    Minor cleanups
    -Added ability labels
    
    -Fixed bug for ability overflow in 'unlocked abilites'
    
    -Added resources for each wisp
    
    -Added custom fonts
    
    -Modular Sprite assignment for the souls

--- Scripts/Items/soul.gd
+++ Scripts/Items/soul.gd
@@ -2,28 +2,31 @@
 extends Node2D
 
 @export var ability_name: AbilityData.ability_list = AbilityData.ability_list.Dash
+@export var custom_sprite_frames: SpriteFrames
+@export var custom_sprite_scale: Vector2 = Vector2(0.225, 0.225)
 
-#const ability_names = [
-	#"Dash",
-	#"Archery",
-	#"Wallsliding",
-	#"DoubleJump",
-	#"WallJump"
-#]
+@onready var ability_name_label: Label = $abilityNameLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	if custom_sprite_frames:
+		$AnimatedSprite2D.sprite_frames = custom_sprite_frames
+		$AnimatedSprite2D.scale = custom_sprite_scale
+		
+		#var texture = $AnimatedSprite2D.sprite_frames.get_frame_texture("default", 0)
+		#var sprite_height = texture.get_height() * $AnimatedSprite2D.scale.y
+		#print("texture: ", texture)
+		#print("sprite height: ", sprite_height)
+		#print()
+		#ability_name_label.position = Vector2(0, sprite_height + 5) 
+	else:
+		$AnimatedSprite2D.scale = Vector2(1,1)
+	$AnimatedSprite2D.play("default")
+	ability_name_label.text = AbilityData.ability_list.keys()[ability_name]
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	
 	if body is Player:
 		AbilityData.add_collected_ability_add_to_list(ability_name)
-		AbilityData.update_debug_ability_label.emit()
-		AbilityData.update_delete_ability_buttons.emit()
-		AbilityData.update_unlock_ability_buttons.emit()
 		self.queue_free()
