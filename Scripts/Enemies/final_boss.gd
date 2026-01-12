1d73cecd1d6169097d1f1999fc8e4449f4c1ee97
DerksenLucas <23111186@student.hhs.nl>
2026-01-12 12:16:56 +0100

    laskdfjlaskdfj

--- Scripts/Enemies/final_boss.gd
+++ Scripts/Enemies/final_boss.gd
@@ -4,7 +4,10 @@ class_name final_boss
 
 @export var jump_speed: int = -125.0
 @onready var JumpTimer : Timer = $JumpTimer
+@onready var WalkTimer: Timer = $WalkTimer
+@onready var ShockwaveArea : CollisionShape2D = $Shockwave/CollisionShape2D
 var just_jumped: bool = false
+var latest_dir : int
 
 func _ready() -> void:
 	projectile = load("res://Scenes/Weapons/enemy_hat_projectile.tscn")
@@ -20,20 +23,18 @@ func _process(_delta: float) -> void:
 	super._process(_delta)
 	if is_on_floor() and !ground.enabled:
 		ground.set_enabled(true)
-	if just_jumped and velocity.y == 0 and !ground.is_colliding():
-		print("skibedi")
-		gravity = 2000
-		shoot()
+	if just_jumped and velocity.y > 0 and !ground.is_colliding():
+		if gravity != 2000:
+			gravity = 2000
 	if just_jumped and ground.is_colliding():
-		print("asklfdj")
 		just_jumped = false
-		can_move = true
 		gravity = 300
+		WalkTimer.start()
+		shoot()
 	if speed != 0:
 		sprite.play("Walking")
 	else:
 		sprite.play("Idle")
-	#print(gravity)
 
 func _on_jump_timer_timeout() -> void:
 	$JumpChecker.position.x = dir*speed
@@ -53,8 +54,7 @@ func pre_shoot():
 	just_jumped = true
 
 func shoot():
-	#print('skibidi')
-	pass
+	ShockwaveArea.set_disabled(false)
 
 func _on_stage_3_timer_timeout() -> void:
 	shoot()
@@ -62,3 +62,12 @@ func _on_stage_3_timer_timeout() -> void:
 	
 func _on_animated_sprite_2d_animation_finished() -> void:
 	sprite.play("Idle")
+
+func _on_walk_timer_timeout() -> void:
+	can_move = true
+	ShockwaveArea.set_disabled(true)
+
+
+func _on_shockwave_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		Player.damage()
